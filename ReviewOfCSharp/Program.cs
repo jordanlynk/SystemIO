0091132bd420d22b4207efa2cc241caae0a3cb6e
Jordan Kidwell <jkid2023@gmail.com>
2021-01-13 16:47:16 -0800

    challenge 3

--- ReviewOfCSharp/Program.cs
+++ ReviewOfCSharp/Program.cs
@@ -6,7 +6,8 @@ namespace ReviewOfCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This was your average, " + AverageOfNumbers(getNumbers()));
+            /* Console.WriteLine("This was your average, " + AverageOfNumbers(getNumbers())); */
+            drawDiamond();
         }
         public static string getNumbers()
         {
@@ -34,8 +35,48 @@ namespace ReviewOfCSharp
             return Decimal.Divide(sum, arrayLengthAsDecimal);
 
         }
+        static void drawDiamond()
+        {
+            char space = ' ';
+            char asterisk = '*';
+            int numberOfSpaces = 4;
+            int numberOfAsterisk = 1;
+            string finalOutput;
 
-
+            for (int i = 1; i < 10; i++)
+            {
+                finalOutput = "";
+                for (int j = 1; j < 10; j++)
+                {
+                    if (j < numberOfSpaces)
+                    {
+                        finalOutput += space;
+                    }
+                    else if (j < numberOfSpaces + numberOfAsterisk)
+                    {
+                        finalOutput += asterisk;
+                    }
+                    else
+                    {
+                        finalOutput += space;
+                    }
+                        
+                }
+                if (i < 5)
+                {
+                    numberOfSpaces--;
+                    numberOfAsterisk += 2;
+                }
+                else
+                {
+                    numberOfSpaces++;
+                    numberOfAsterisk -= 2;
+                }
+                Console.WriteLine(finalOutput);
+                
+            }
+        }
     }
+
 
 }

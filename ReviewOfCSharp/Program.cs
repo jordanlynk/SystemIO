737dd3614cb26b86a5ea981a2bfa6380e3997a2e
Jordan Kidwell <jkid2023@gmail.com>
2021-01-13 16:09:39 -0800

    added in challenge 2 so far

---
+++ ReviewOfCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ReviewOfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This was your average, " + AverageOfNumbers(getNumbers()));
+        }
+        public static string getNumbers()
+        {
+            Console.WriteLine("Please enter a number between 2 and 10");
+            return Console.ReadLine();
+        }
+        public static decimal AverageOfNumbers(string userNum)
+        {   
+            // converting parameter to an integer
+            int arrayLength = Convert.ToInt32(userNum);
+            decimal[] averageArray = new decimal[arrayLength];
+            decimal sum = 0;
+            // every time it iterates through, it assigns each index to the value input by the user
+            for (int i = 0; i < averageArray.Length; i++)
+            {
+                Console.WriteLine("Please enter a random number.");
+                averageArray[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+
+            foreach (decimal num in averageArray)
+            {
+                sum += num;
+            }
+            decimal arrayLengthAsDecimal = Convert.ToDecimal(arrayLength);
+            return Decimal.Divide(sum, arrayLengthAsDecimal);
+
+        }
+
+
+    }
+
+}
